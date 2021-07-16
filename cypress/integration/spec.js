c3710c64a6b09e9d9a7e72b8d82ba5d76b8ec4e7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-07-16 15:14:08 -0400

    add test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -7,4 +7,14 @@ describe('Next.js app', () => {
       'be.visible',
     )
   })
+
+  it('shows the text from the __NEXT_DATA__', () => {
+    cy.visit('/')
+      // visit yields the "window" object
+      // and we can get nested property in a single command
+      .its('__NEXT_DATA__.props.pageProps.experiments.greeting')
+      .then((greeting) => {
+        cy.contains('[data-cy=greeting]', greeting).should('be.visible')
+      })
+  })
 })
